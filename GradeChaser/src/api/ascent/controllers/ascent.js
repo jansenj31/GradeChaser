30816a962d5d88c1e66efc57a4d04a6c531e10a3
Jonas Jansen <46447158+jansenj31@users.noreply.github.com>
2024-01-02 10:37:12 +0100

    Strapi setup

---
+++ GradeChaser/src/api/ascent/controllers/ascent.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ascent controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::ascent.ascent');
---
+++ GradeChaser/src/api/ascent/routes/ascent.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ascent router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::ascent.ascent');
